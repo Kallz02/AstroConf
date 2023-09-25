b00e78b02d03da37827081538831781c7e336d4e
Kallz02 <akshay.pranav.kalathil@gmail.com>
2023-09-25 20:15:06 +0530

    update

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,6 +5,7 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.github-nvim-theme" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.java" },
   { import = "astrocommunity.pack.tailwindcss" },

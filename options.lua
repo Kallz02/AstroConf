636473fad73f9419a2a9f716e490d920385c9517
Kallz02 <akshay.pranav.kalathil@gmail.com>
2023-10-24 22:56:34 +0530

    update

--- options.lua
+++ options.lua
@@ -8,6 +8,7 @@ return {
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
     guifont = "JetBrainsMono Nerd Font:h13"
+    -- title= "neovide"
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
